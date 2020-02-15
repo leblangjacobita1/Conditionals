974acea0e05e3e79e6ae0cf9229b1a8301a9d62d
leblangjacobita1 <57274817+leblangjacobita1@users.noreply.github.com>
2020-02-14 19:39:02 -0600

    Initial commit

---
+++ Work/Conditional_Statement_Drill.py
@@ -0,0 +1,49 @@
+'''
+Created on Feb 14, 2020
+
+@author: ITAUser
+'''
+
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+'''
+
+'''
+#1) Create a Variable called grade and set it to an integer. Make an if statement that checks if the grade is a passing grade. grade must be above 65 to pass. print out "student is passing"
+'''
+grade=66
+if (grade>65):
+        print("student is passing")
+'''
+#2) Now make an if, else statement that checks if the student is passing but also print "student is failing", if the grade is less than 65
+'''
+if (grade>65):
+        print("student is passing")
+else:
+    print("student is failing")
+'''
+#3)Create a variable called age. Make and if, else statement that checks if the age entered is old enough to vote. Remember the voting age is 18
+'''
+age=17
+if (age<18):
+    print("Too young, get outta here")
+else:
+    print("You're good, and can vote")
+'''
+#4)Create a variable called weight. Make an if statement that checks the unit of the weight. If the weight is in kilograms, convert it to pounds 
+'''
+weight = "Pounds"
+if (weight=="Pounds"):
+        print("Kilograms")
+        
+'''
+#5)Now modify the previous program to also convert from pounds to kilograms
+'''
+weight = "Kilograms"
+if (weight=="Kilograms"):
+        print("Pounds")
+   
+        
+        
+        
+    
